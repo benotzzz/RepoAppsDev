48c106853f63f9fe110dd36b28edfa60fa5cfea2
Mervyn Morales <mervynmorales@gmail.com>
2021-09-14 09:36:31 +0800

    AddedCalculator

---
+++ Calculator/script.js
@@ -0,0 +1,30 @@
+
+const number = prompt('Choose ( 1: Addition, 2: Subtraction,3: Mutiplication 4: Division ): ');
+
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+
+
+
+if (number == '1') {
+    alert (number1 + number2);
+}
+else if (number == '2') {
+   alert (number1 - number2);
+}
+else if (number == '3') {
+    alert (number1 * number2);
+}
+else if (number == '4') {
+    alert (number1 / number2);
+    
+}
+else {
+    (number )
+
+}
+
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
